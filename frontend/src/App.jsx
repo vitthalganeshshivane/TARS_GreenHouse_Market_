157562a6cfd449b9ed16db67c414f0a0aea9f48c
ritishDas <ritishdas116@outlook.com>
2026-04-07 12:45:19 +0530

    add:topbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,28 +5,33 @@ import { Recycle } from 'lucide-react'
 import CategoryEntry from './components/categoryEntry.jsx'
 import CategoryCard from './components/categoryCard.jsx'
 import ProductCard from './components/productCard.jsx'
+import Navbar from './components/navbar.jsx'
+import Topbar from './components/pages/home/topbar.jsx'
 
 function App() {
 
   return (
-    <>
-      <SearchProduct />
-
-      <NavbarBtn icon={<Recycle />} text='Compare' badge={{ status: true, value: 5 }} link='https://ritish.site' />
-
-      <CategoryCard title='Category' >
-        <CategoryEntry icon={<Recycle />} text='compare' number='4' link='https://ritish.site' />
-      </CategoryCard>
-
-      <ProductCard image='' category='Fresh Food' name='Organic Red Rice' rating={4} brand="NestFood" price={200} discount={5} />
-
-
-
-
-    </>
+    <Topbar />
   )
 }
 
 export default App
 
 
+// Component Usage
+// <>
+//   <SearchProduct />
+//
+//   <NavbarBtn icon={<Recycle />} text='Compare' badge={{ status: true, value: 5 }} link='https://ritish.site' />
+//
+//   <CategoryCard title='Category' >
+//     <CategoryEntry icon={<Recycle />} text='compare' number='4' link='https://ritish.site' />
+//   </CategoryCard>
+//
+//   <ProductCard image='https://www.sirimart.in/wp-content/uploads/2023/12/Organic_Red_Rice.jpg' ratingCount={20} category='Fresh Food' name='Organic Red Rice' rating={4} brand="NestFood" price={200} discount={5} sale={true} />
+//
+//
+//   <Navbar />
+//
+//
+// </>
