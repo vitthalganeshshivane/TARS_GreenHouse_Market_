d297e19a2bd008d77e919b46e7bade1e8e124fb4
vitthal4545 <vitthalgs1434@gmail.com>
2026-04-08 13:43:21 +0530

    feat(backend/product): extend product schema with inventory, pricing, metadata, and rating support

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -5,9 +5,24 @@ const productSchema = new mongoose.Schema(
     title: {
       type: String,
       required: true,
+      trim: true,
     },
 
-    description: String,
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    brand: {
+      type: String, // Seeds of Change
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    subCategory: String,
 
     price: {
       type: Number,
@@ -16,18 +31,75 @@ const productSchema = new mongoose.Schema(
 
     discountPrice: Number,
 
-    category: String,
-
     stock: {
       type: Number,
       required: true,
+      default: 0,
+    },
+
+    // Stock Keeping Unit
+    // --> It’s a unique identifier for each product
+    // Example:
+    // RICE-ORG-1KG-001
+    // MILK-AMUL-500ML-002
+
+    sku: {
+      type: String,
+      unique: true,
+    },
+
+    mfgDate: Date,
+
+    life: {
+      type: Number, // 70 days
+    },
+
+    type: {
+      type: String, // Organic
     },
 
     images: [String],
 
+    thumbnail: String,
+
+    sizes: [String], // 50g, 60g etc.
+
+    colors: [
+      {
+        name: String,
+        stock: Number,
+      },
+    ],
+
+    ratings: {
+      average: {
+        type: Number,
+        default: 0,
+      },
+      count: {
+        type: Number,
+        default: 0,
+      },
+    },
+
+    tags: [String],
+
     vendor: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
+    },
+
+    additionalInfo: {
+      packaging: String,
+      ingredients: String,
+      warnings: String,
+      suggestedUse: String,
+    },
+
+    isFeatured: {
+      type: Boolean,
+      default: false,
     },
 
     isAvailable: {
