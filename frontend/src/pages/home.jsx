4686e088938f97586a14190fb3ea64909d16a839
omparkhi <omparkhi23@gmail.com>
2026-04-08 11:29:03 +0530

    integrate Home page with home route

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,4 +1,9 @@
 import { useAuth } from "../hooks/useAuth";
+import Topbar from "../components/pages/home/topbar.jsx";
+import Navbar from "../components/pages/home/navbar.jsx";
+import ImageSlider from "../components/imageSlider.jsx";
+import PopularProduct from "../components/pages/home/popularProduct.jsx";
+import DealsOfDay from "../components/dealsOfDayComponent.jsx";
 
 export default function Home() {
   const { user, loading } = useAuth();
@@ -8,10 +13,32 @@ export default function Home() {
   if (!user) return <p>Unauthorized</p>;
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
+    <div className="bg-white min-h-screen">
+      <Topbar />
+      <Navbar />
+
+      <div className="px-2 sm:px-4 md:px-6 lg:px-8 py-2 md:py-4">
+        <ImageSlider
+          slides={[
+            {
+              href: "ritish.site",
+              src: "https://static.vecteezy.com/system/resources/thumbnails/004/948/401/small/organic-shop-poster-finished-design-trolley-with-vegetables-vector.jpg",
+            },
+            {
+              href: "ritish.site",
+              src: "https://static.vecteezy.com/system/resources/thumbnails/004/948/401/small/organic-shop-poster-finished-design-trolley-with-vegetables-vector.jpg",
+            },
+            {
+              href: "ritish.site",
+              src: "https://static.vecteezy.com/system/resources/thumbnails/004/948/401/small/organic-shop-poster-finished-design-trolley-with-vegetables-vector.jpg",
+            },
+          ]}
+        />
+      </div>
+
+      <PopularProduct />
+
+      <DealsOfDay />
     </div>
   );
 }
