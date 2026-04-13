9bd52116c74a38e64692ab98259e4c4d503066aa
vitthal4545 <vitthalgs1434@gmail.com>
2026-04-13 11:39:49 +0530

    feat(backend/core): enhance user and order models, add notification system and counter for custom order IDs

---
+++ backend/models/Counter.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const counterSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    value: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Counter", counterSchema);
