5d4bb1e2a3de458f7368e7591f09a4ebc09ca673
vitthal4545 <vitthalgs1434@gmail.com>
2026-04-13 11:53:58 +0530

    feat(backend/order): implement order processing with stock management, notifications, and role-based access

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import productRoutes from "./routes/product.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import vendorRoutes from "./routes/vendor.routes.js";
 import addressRoutes from "./routes/address.routes.js";
+import orderRoutes from "./routes/order.routes.js";
 
 const app = express();
 const PORT = 3000;
@@ -30,6 +31,7 @@ app.use("/api/product", productRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/vendor", vendorRoutes);
 app.use("/api/address", addressRoutes);
+app.use("/api/order", orderRoutes);
 
 // GLOBAL ERROR HANDLER
 app.use((err, req, res, next) => {
