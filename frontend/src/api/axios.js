7100ff35af21ccbb2db82fb5e37a1d4e49a4ca60
vitthal4545 <vitthalgs1434@gmail.com>
2026-04-30 16:52:58 +0530

    fix(api): use env-based API base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/token";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 API.interceptors.request.use((req) => {
